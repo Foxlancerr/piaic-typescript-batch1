46a4b56a2ff84e92fc35fe0eba201f59ff6bcef3
M-Asimm <kpkprince55persia@gmail.com>
2024-01-05 10:02:41 +0500

    some chanhes

--- 04-presentation2/index.ts
+++ 04-presentation2/index.ts
@@ -17,9 +17,6 @@ console.log(fruit);
 fruit.sort((a: number, b: number) => a - b);
 console.log(fruit);
 
-
-
-
 /**
  *  @param {Reduce arrays methods}
  * ***********************************************************************
@@ -58,18 +55,61 @@ const reducer = (accumulator: number, item: number) => {
   and our initial value */
 const total = num2.reduce(reducer, initialValue);
 
-
 // Flattening an Array Using reducer
 const numArray = [1, 2, [3, 10, [11, 12]], [1, 2, [3, 4]], 5, 6];
 
-function flattenArray(data:any) {
+function flattenArray(data: any) {
   // our initial value this time is a blank array
-  let initialValue:any = [];
+  let initialValue: any = [];
 
   // call reduce on our data
-  return data.reduce((total:any, value:number) => {
+  return data.reduce((total: any, value: number) => {
     // if the value is an array then recursively call reduce
     // if the value is not an array then just concat our value
     return total.concat(Array.isArray(value) ? flattenArray(value) : value);
   }, initialValue);
 }
+
+/**
+ *  @param {Some arrays methods}
+ * ***********************************************************************
+ * */
+// The some() method tests whether any of the array elements pass the given test function.
+// arr.some(callback(currentValue))
+// returns boolean
+
+// array of scores obtained by student
+let scoreObtained = [45, 50, 39, 78, 65, 20];
+
+// a test function: returns score less than 40
+function studentIsPassed(score: number) {
+  return score < 40;
+}
+
+// checks if score of at least one student is less than 40
+if (scoreObtained.some(studentIsPassed) == true) {
+  console.log("At least one of the students failed.");
+} else console.log("All students are passed.");
+
+/**
+ *  @param {Every arrays methods}
+ * ***********************************************************************
+ * */
+// The every() method checks if all the array elements pass the given test function.
+// arr.some(callback(currentValue))
+// returns boolean
+
+
+// function that checks whether
+// the age is 18 or above
+function checkAdult(age:number) {
+  return age >= 18;
+}
+
+const ageArray = [34, 23, 20, 26, 12];
+
+//checks if all the array elements
+// pass the checkAdult() function
+let check = ageArray.every(checkAdult);
+
+// Output: false
