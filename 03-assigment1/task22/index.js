e8e9cf0feb8512791a4f45aeb2394beded3a3d4a
M-Asimm <kpkprince55persia@gmail.com>
2024-01-19 11:28:17 +0500

    task22 error is fixed

--- 03-assigment1/task22/index.js
+++ 03-assigment1/task22/index.js
@@ -2,15 +2,12 @@
  * @param {Question 22}
  * Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
  */
-
 //Intentianal Error is such type of Error like Bug or Mistake (out of bounds index value access)
-
-let marks: number[] = [20, 34, 56, 90];
-
-let incorrectIndex: number = 10;
-
+var marks = [20, 34, 56, 90];
+var incorrectIndex = 10;
 if (incorrectIndex > 0 && incorrectIndex < marks.length) {
-  console.log("The marks at Specific index is: " + marks[incorrectIndex]);
-} else {
-  console.log("out of bounds (no index present)");
+    console.log("The marks at Specific index is: " + marks[incorrectIndex]);
+}
+else {
+    console.log("out of bounds (no index present)");
 }
