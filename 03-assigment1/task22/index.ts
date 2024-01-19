4a5352402494f9cfa7e06e19e7b46db25831e61a
M-Asimm <kpkprince55persia@gmail.com>
2024-01-19 11:25:35 +0500

    task 22 is done

---
+++ 03-assigment1/task22/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @param {Question 22}
+ * Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+ */
+
+//Intentianal Error is such type of Error like Bug or Mistake (out of bounds index value access)
+
+let marks: number[] = [20, 34, 56, 90];
+
+let incorrectIndex: number = 10;
+
+if (incorrectIndex > 0 && incorrectIndex < marks.length) {
+  console.log("The marks at Specific index is: " + marks[incorrectIndex]);
+}else{
+    throw new Error("out of bounds (no index present)")
+}
