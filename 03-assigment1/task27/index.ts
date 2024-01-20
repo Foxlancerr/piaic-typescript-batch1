74a4c9014820e9c55d9e49f2edec258e997c6789
M-Asimm <kpkprince55persia@gmail.com>
2024-01-20 14:43:15 +0500

    task27 is done

---
+++ 03-assigment1/task27/index.ts
@@ -0,0 +1,28 @@
+/**
+ * @param {Question27}
+ * Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+• If the alien is green, print a message that the player earned 5 points.
+
+• If the alien is yellow, print a message that the player earned 10 points.
+
+• If the alien is red, print a message that the player earned 15 points.
+
+• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+ */
+
+function alien(color: string): void {
+  if (color === "green") {
+    console.log("You just earned 15 points for shooting the green alien.");
+  } else if (color === "yellow") {
+    console.log("You just earned 10 points for shooting the yellow alien.");
+  } else if (color === "red") {
+    console.log("You just earned 5 points for shooting the yellow alien.");
+  } else {
+    console.log("Nice try, but you don't have earned any points for shooting");
+  }
+}
+
+alien("green");
+alien("red");
+alien("green");
+alien("red");
