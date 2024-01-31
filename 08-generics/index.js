e3967495986ed9db2219572b68e713c0613c13bc
M-Asimm <kpkprince55persia@gmail.com>
2024-01-31 21:54:42 +0500

    generics more advanced

--- 08-generics/index.js
+++ 08-generics/index.js
@@ -1,37 +1,72 @@
-"use strict";
 /**
  * @param { Generics}
  *
  * Generics are a fundamental feature of statically-typed languages, allowing developers to pass types as parameters to another type, function, or other structure. When a developer makes their component a generic component, they give that component the ability to accept and enforce typing that is passed in when the component is used, which improves code flexibility, makes components reusable, and removes duplication.
  */
 // objects
-let student1 = {
+var student1 = {
     id: "1",
     marks: ["40", "50"],
 };
-let student2 = {
+var student2 = {
     id: 10,
     marks: [100, 70],
 };
 // function
 function getStudent(id, name) {
-    console.log(id, name);
+    // console.log(id, name);
 }
-getStudent(1, "kamran");
+// getStudent(1, "kamran");
 getStudent(1, "kamran"); // type is passed as parameter
 function identity(value) {
     return value;
 }
-const result = identity({ name: "kamran" });
-console.log(result);
-const boolCheck = (args) => {
+var result = identity({ name: "kamran" });
+// console.log(result);
+var boolCheck = function (args) {
     if (Array.isArray(args) && args.length)
-        return { args, is: false };
+        return { args: args, is: false };
     if (typeof args === "object" && args !== null && !Object.keys(args))
-        return { args, is: false };
-    return { args, is: !!args };
+        return { args: args, is: false };
+    return { args: args, is: !!args };
 };
-console.log(boolCheck("kamran"));
-console.log(boolCheck([]));
-console.log(boolCheck(""));
-console.log(boolCheck({ name: "ahmad" }));
+var processUser = function (user) {
+    return user;
+};
+processUser({ id: 12, name: "ahmad" });
+// processUser({  name: "ahmad" }); //not allowed
+// function loggingIdentity<Type>(arg: Type): Type {
+// console.log(arg.length);
+// Property 'length' does not exist on type 'Type'.
+// return arg;
+// }
+// let logIdentity = loggingIdentity([34, 45, 34, 67]);
+// console.log(typeof logIdentity);
+function identity1(arg) {
+    return arg;
+}
+// way1
+var myIdentity1 = identity1;
+console.log(myIdentity1("hii"));
+console.log(typeof myIdentity1);
+// way2
+var myIdentity2 = identity1;
+console.log(myIdentity2("hii"));
+console.log(typeof myIdentity2);
+var stuObj1 = {
+    name: "sudais",
+    age: 45,
+};
+// generics for classes
+var Animals = /** @class */ (function () {
+    function Animals() {
+    }
+    return Animals;
+}());
+var dog1 = new Animals();
+// dog1.name = 34 // not allowed
+dog1.name = "Dog1";
+dog1.bark = function (voice) {
+    console.log(voice);
+};
+dog1.bark("woo woo");
--- 08-generics/index.ts
+++ 08-generics/index.ts
@@ -22,7 +22,7 @@ let student2: Box<number> = {
 
 // function
 function getStudent<T, K>(id: T, name: K) {
-  console.log(id, name);
+  // console.log(id, name);
 }
 
 // getStudent(1, "kamran");
@@ -41,13 +41,95 @@ const result = identity<ProgrammingLanguage>({ name: "kamran" });
 
 const boolCheck = <T>(args: T): { args: T; is: boolean } => {
   if (Array.isArray(args) && args.length) return { args, is: false };
-  if (typeof args === "object" && args !== null && !Object.keys(args as keyof T))
+  if (typeof args === "object" && args !== null && !Object.keys(args))
     return { args, is: false };
   return { args, is: !!args };
 };
 
 // in here we can see differnt datatypes and automatically it converts it
-console.log(boolCheck("kamran"));
-console.log(boolCheck([]));
-console.log(boolCheck(""));
-console.log(boolCheck({ name: "ahmad" }));
+// console.log(boolCheck("kamran"));
+// console.log(boolCheck([]));
+// console.log(boolCheck(""));
+// console.log(boolCheck({ name: "ahmad" }));
+
+interface HasId {
+  id: number;
+}
+
+const processUser = <U extends HasId>(user: U): U => {
+  return user;
+};
+
+processUser({ id: 12, name: "ahmad" });
+// processUser({  name: "ahmad" }); //not allowed
+
+// function loggingIdentity<Type>(arg: Type): Type {
+// console.log(arg.length);
+// Property 'length' does not exist on type 'Type'.
+// return arg;
+// }
+
+// let logIdentity = loggingIdentity([34, 45, 34, 67]);
+// console.log(typeof logIdentity);
+
+// how i solve this issue, by the below codings we can resolve it
+interface GLength {
+  length: number;
+}
+const stuFunction: <T extends GLength>(args: T) => T = (args) => {
+  args.length;
+  return args;
+};
+
+function identity1<Type>(arg: Type): Type {
+  return arg;
+}
+
+// way1
+let myIdentity1: <Type>(arg: Type) => Type = identity1;
+console.log(myIdentity1("hii"));
+console.log(typeof myIdentity1);
+
+// way2
+let myIdentity2: { <Type>(arg: Type): Type } = identity1;
+console.log(myIdentity2("hii"));
+console.log(typeof myIdentity2);
+
+// how i will assign generics for interface interfaces
+
+interface GenericIdentityFn<Type> {
+  (arg: Type): Type;
+}
+
+// function identity<Type>(arg: Type): Type {
+//   return arg;
+// }
+
+// let myIdentity: GenericIdentityFn<number> = identity;
+
+// how i pass type to interface
+// interface Student<S, N> {
+//   name: S;
+//   age: N;
+// }
+
+// let stuObj1: Student<string, number> = {
+//   name: "sudais",
+//   age: 45,
+// };
+
+// generics for classes
+
+// class Animals<S> {
+//   name!: S;
+//   bark!: (voice: string) => void;
+// }
+
+// let dog1 = new Animals<string>();
+
+// dog1.name = 34 // not allowed
+// dog1.name = "Dog1";
+// dog1.bark = function (voice) {
+//   console.log(voice);
+// };
+// dog1.bark("woo woo");
